311eb58f47937c118f91210c3ecce0052e3ec950
dikshitha <dikshitah1713@gmail.com>
2026-09-25 13:34:31 +0530

    add saucedemo file

---
+++ testing/saucedemo.spec.ts
@@ -0,0 +1,8 @@
+import {test, expect} from '@playwright/test'
+
+test("sauces login validation", async ({page}) =>{
+    await page.goto("https://sauce-demo.myshopify.com/account/login")
+    await page.locator("#customer_email").fill("dikshitha1713@gmail.com")
+    await page.locator("#customer_password").fill("testing@123")
+    await page.locator('[value="Sign In"]').click()
+})
