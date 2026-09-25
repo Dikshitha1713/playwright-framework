74e2f831bd20791e027268e428732f3ad63da361
dikshitha <dikshitah1713@gmail.com>
2026-09-25 15:59:32 +0530

    modifided saucedemo file

--- testing/saucedemo.spec.ts
+++ testing/saucedemo.spec.ts
@@ -4,6 +4,6 @@ test("sauces login validation", async ({page}) =>{
     await page.goto("https://sauce-demo.myshopify.com/account/login")
     await page.locator("#customer_email").fill("dikshitha1713@gmail.com")
     await page.locator("#customer_password").fill("testing@123")
-    await page.locator('[value="Sign In"]').click()
+    await page.locator('[value="Sign In"]').first().click()
     await page.getByRole("button",{name:"Sign up"}).first().click()
 })
