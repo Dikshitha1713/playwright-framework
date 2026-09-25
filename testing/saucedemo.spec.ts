9636028b55e783290a88b59f17d98b23b12b3267
dikshitha <dikshitah1713@gmail.com>
2026-09-25 13:53:50 +0530

    added signup faeture to sacedemo file

--- testing/saucedemo.spec.ts
+++ testing/saucedemo.spec.ts
@@ -5,4 +5,5 @@ test("sauces login validation", async ({page}) =>{
     await page.locator("#customer_email").fill("dikshitha1713@gmail.com")
     await page.locator("#customer_password").fill("testing@123")
     await page.locator('[value="Sign In"]').click()
+    await page.getByRole("button",{name:"Sign up"}).first().click()
 })
